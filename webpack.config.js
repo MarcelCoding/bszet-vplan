6d1aade9ad874feacd76d1387251215a71a5a1dc
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-09-12 12:19:32 +0200

    Moved to typescript

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
